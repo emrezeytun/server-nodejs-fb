e2538cd3f257bcd77c6989c57067a2b9eb7c6c7d
emrezeytun <emrezeytun35@gmail.com>
2023-10-29 01:31:01 +0300

    main || log changed

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const db = () => {
+    console.log('connecting...')
     mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -8,7 +9,7 @@ const db = () => {
         console.log('mongodb connect')
     }).catch((err) => {
         // throw new Error(err.message)
-        console.log(err)
+        console.log('connetction error', err)
     } )
 }
 
